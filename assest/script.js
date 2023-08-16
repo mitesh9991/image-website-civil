7f0c39b877f513a687a19b9dc21958691ca5fea9
mitesh bhagwant <miteshbhagwant@gmail.com>
2023-08-16 16:16:46 +0530

    plan added1

--- assest/script.js
+++ assest/script.js
@@ -1,20 +1,20 @@
-const mobileHam = document.getElementById('mobileHam');
-const mobileNav = document.getElementById('mobileNav');
-
-mobileHam.addEventListener('click', mobileNavClick);
-
-
-function mobileNavClick(){
-    console.log("clicked")
-
-    mobileNav.classList.toggle('hidden')
-    mobileNav.classList.toggle('flex')
+const mobileHam = document.getElementById("mobileHam");
+const mobileNav = document.getElementById("mobileNav");
+const mobileNavOpen = document.getElementById('mobileOpen');
+const mobileNavHamClose = document.getElementById("mobileNavHamClose");
+const main = document.getElementById("main");
 
 
+
+mobileHam.addEventListener("click", mobileNavClick);
+
+function mobileNavClick() {
+  mobileNav.classList.toggle("hidden");
+  mobileNav.classList.toggle("flex");
+
+  mobileNavOpen.classList.toggle("hidden");
+  mobileNavHamClose.classList.toggle("hidden");
 }
-
-
-
 
 // Get all sections that have an ID defined
 const sections = document.querySelectorAll("section[id]");
@@ -23,28 +23,27 @@ const sections = document.querySelectorAll("section[id]");
 window.addEventListener("scroll", navHighlighter);
 
 function navHighlighter() {
-  
-    
   // Get current scroll position
   let scrollY = window.pageYOffset;
-  
+
   // Now we loop through sections to get height, top and ID values for each
-  sections.forEach(current => {
+  sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 20;
     const sectionId = current.getAttribute("id");
- 
+
     /*
     - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
     - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
     */
-    if (
-      scrollY > sectionTop &&
-      scrollY <= sectionTop + sectionHeight
-    ){
-      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".navigation a[href*=" + sectionId + "]")
+        .classList.add("active");
     } else {
-      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+      document
+        .querySelector(".navigation a[href*=" + sectionId + "]")
+        .classList.remove("active");
     }
   });
 }
